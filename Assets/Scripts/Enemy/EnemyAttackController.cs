85e49d2a1cc84b7bb3537f03a69853d08ca0266e
JiaChen ZENG <jiachen@zeng.games>
2021-12-26 02:03:55 +0800

    FEAT: #36 雑魚敵の視野を実装

---
+++ Assets/Scripts/Enemy/EnemyAttackController.cs
@@ -0,0 +1,68 @@
+﻿using BulletHell;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// 敵の視野に入ってから攻撃を始めることを制御する
+/// </summary>
+public class EnemyAttackController : MonoBehaviour
+{
+    /// <summary>
+    /// 敵の視野半径
+    /// </summary>
+    [SerializeField] private float m_fieldOfView = 5;
+    private GameObject m_fov;
+
+    [SerializeField] private GameObject[] m_danmakuList;
+
+    private void Awake()
+    {
+        InitFOV();
+        InitDanmakus();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void InitFOV()
+    {
+        m_fov = Instantiate(Resources.Load<GameObject>("Prefabs/EnemyNormalFOV"), transform);
+        var fov = m_fov.GetComponent<EnemyFOV>();
+        fov.FOV = m_fieldOfView;
+        fov.OnEnterFOV.AddListener(OnEnterFOV);
+        fov.OnExitFOV.AddListener(OnExitFOV);
+    }
+
+    private void InitDanmakus()
+    {
+        m_danmakuList = m_danmakuList.Select(danmaku =>
+        {
+            danmaku = Instantiate(danmaku, transform);
+            danmaku.GetComponent<ProjectileEmitterAdvanced>().AutoFire = false;
+            return danmaku;
+        }).ToArray();
+    }
+
+    private void OnEnterFOV(GameObject player)
+    {
+        Debug.Log("Enter FOV");
+        foreach (var danmaku in m_danmakuList)
+        {
+            var emitter = danmaku.GetComponent<ProjectileEmitterAdvanced>();
+            emitter.AutoFire = true;
+            emitter.Target = player.transform;
+        }
+    }
+
+    private void OnExitFOV(GameObject player)
+    {
+        Debug.Log("Exit FOV");
+        foreach (var danmaku in m_danmakuList)
+        {
+            var emitter = danmaku.GetComponent<ProjectileEmitterAdvanced>();
+            emitter.AutoFire = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyFOV.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 敵の視野に入ったかどうかを事件で知らせる
+/// </summary>
+public class EnemyFOV : MonoBehaviour
+{
+    public readonly UnityEvent<GameObject> OnEnterFOV = new UnityEvent<GameObject>();
+    public readonly UnityEvent<GameObject> OnExitFOV = new UnityEvent<GameObject>();
+
+    public float FOV
+    { 
+        get => GetComponent<CircleCollider2D>().radius;
+        internal set { GetComponent<CircleCollider2D>().radius = value; }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+
+        OnEnterFOV.Invoke(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+
+        OnExitFOV.Invoke(collision.gameObject);
+    }
+}
