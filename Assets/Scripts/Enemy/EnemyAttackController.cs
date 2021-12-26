37d6a9ebffa73d11dfd14a6eb521fe61965c9739
JiaChen ZENG <jiachen@zeng.games>
2021-12-26 14:49:37 +0800

    FEAT: Close #37 複数の弾幕パターンが変更できるようにする

--- Assets/Scripts/Enemy/EnemyAttackController.cs
+++ Assets/Scripts/Enemy/EnemyAttackController.cs
@@ -1,5 +1,6 @@
 ﻿using BulletHell;
 using System;
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 
@@ -24,7 +25,21 @@ public class EnemyAttackController : MonoBehaviour
     [SerializeField] private float m_fieldOfView = 5;
     private GameObject m_fov;
 
+    /// <summary>
+    /// 弾幕パターンが事前に設定しておいたプレハブを置くリスト
+    /// </summary>
     [SerializeField] private GameObject[] m_danmakuList;
+
+    /// <summary>
+    /// 弾幕パターンリストの中の弾幕のそれぞれの持続時間
+    /// </summary>
+    [SerializeField] private float[] m_danmakuDurations;
+
+    /// <summary>
+    /// 現在発射している弾幕パターンの添え字。-1 は現在発射していないことを意味する。
+    /// </summary>
+    private int m_currentDanmakuIndex = -1;
+    private IEnumerator m_currentDanmakuRoutine;
 
     private void Awake()
     {
@@ -53,26 +68,64 @@ public class EnemyAttackController : MonoBehaviour
         m_danmakuList = m_danmakuList.Select(danmaku => Instantiate(danmaku, transform)).ToArray();
     }
 
-    private void OnEnterFOV(GameObject player)
+    private void StartDanmaku(Transform playerTransform)
+    {
+        SetDanmakuTarget(playerTransform);
+        m_currentDanmakuRoutine = DanmakuRoutine();
+        if (m_danmakuDurations.Length > 0) StartCoroutine(m_currentDanmakuRoutine);
+        else
+        {
+            m_currentDanmakuIndex = 0;
+            SetDanmakuEnabled(m_currentDanmakuIndex, true);
+        }
+    }
+
+    private void StopDanmaku()
+    {
+        SetDanmakuEnabled(m_currentDanmakuIndex, false);
+        m_currentDanmakuIndex = -1;
+        StopCoroutine(m_currentDanmakuRoutine);
+    }
+
+    private IEnumerator DanmakuRoutine()
+    {
+        m_currentDanmakuIndex = 0;
+        SetDanmakuEnabled(m_currentDanmakuIndex, true);
+        while (true)
+        {
+            yield return new WaitForSeconds(m_danmakuDurations[m_currentDanmakuIndex]);
+            SetDanmakuEnabled(m_currentDanmakuIndex, false);
+            m_currentDanmakuIndex = (m_currentDanmakuIndex + 1) % m_danmakuList.Length;
+            SetDanmakuEnabled(m_currentDanmakuIndex, true);
+        }
+    }
+
+    private void SetDanmakuTarget(Transform playerTransform)
     {
         foreach (var danmaku in m_danmakuList)
         {
             foreach (var emitter in danmaku.GetComponents<ProjectileEmitterAdvanced>())
             {
-                emitter.AutoFire = true;
-                emitter.Target = player.transform;
+                emitter.Target = playerTransform;
             }
         }
     }
 
+    private void SetDanmakuEnabled(int index, bool enabled)
+    {
+        foreach (var emitter in m_danmakuList[index].GetComponents<ProjectileEmitterAdvanced>())
+        {
+            emitter.AutoFire = enabled;
+        }
+    }
+
+    private void OnEnterFOV(GameObject player)
+    {
+        StartDanmaku(player.transform);
+    }
+
     private void OnExitFOV(GameObject player)
     {
-        foreach (var danmaku in m_danmakuList)
-        {
-            foreach (var emitter in danmaku.GetComponents<ProjectileEmitterAdvanced>())
-            {
-                emitter.AutoFire = false;
-            }
-        }
+        StopDanmaku();
     }
 }
