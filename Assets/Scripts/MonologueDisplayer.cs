c5415f9df36f93639dee777467855e1887214c43
lyn-teambeta <86309684+lyn-teambeta@users.noreply.github.com>
2021-12-27 00:18:32 +0900

    イベント
    Monologueのプレハブ実装

---
+++ Assets/Scripts/MonologueDisplayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonologueDisplayer : MonoBehaviour
+{
+    bool isCalledOnce = false;
+
+    [SerializeField] string Monologue = "";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isCalledOnce)
+        {
+            isCalledOnce = true;
+            Debug.Log(Monologue);
+        }
+    }
+}
