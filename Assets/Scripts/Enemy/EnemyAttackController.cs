7527fc0a2bbf4ac510f20a535fc72f41ec494c1f
JiaChen ZENG <jiachen@zeng.games>
2021-12-26 13:41:24 +0800

    FEAT: Close #36 ボスの視野を実装
    Signed-off-by: JiaChen ZENG <jiachen@zeng.games>

--- Assets/Scripts/Enemy/EnemyAttackController.cs
+++ Assets/Scripts/Enemy/EnemyAttackController.cs
@@ -1,4 +1,5 @@
 ﻿using BulletHell;
+using System;
 using System.Linq;
 using UnityEngine;
 
@@ -7,6 +8,16 @@ using UnityEngine;
 /// </summary>
 public class EnemyAttackController : MonoBehaviour
 {
+    /// <summary>
+    /// 敵の種類によって攻撃の AI が違う
+    /// </summary>
+    [SerializeField] private bool m_isBoss;
+
+    /// <summary>
+    /// ボスの固有領域
+    /// </summary>
+    [SerializeField] private EnemyBossFOV m_intrinsicArea;
+
     /// <summary>
     /// 敵の視野半径
     /// </summary>
@@ -17,52 +28,51 @@ public class EnemyAttackController : MonoBehaviour
 
     private void Awake()
     {
-        InitFOV();
+        if (m_isBoss) InitBossFOV();
+        else InitNormalFOV();
         InitDanmakus();
     }
 
-    private void Update()
+    private void InitBossFOV()
     {
-
+        m_intrinsicArea.OnEnterFOV.AddListener(OnEnterFOV);
+        m_intrinsicArea.OnExitFOV.AddListener(OnExitFOV);
     }
 
-    private void InitFOV()
+    private void InitNormalFOV()
     {
         m_fov = Instantiate(Resources.Load<GameObject>("Prefabs/EnemyNormalFOV"), transform);
-        var fov = m_fov.GetComponent<EnemyFOV>();
-        fov.FOV = m_fieldOfView;
+        var fov = m_fov.GetComponent<EnemyNormalFOV>();
+        fov.Radius = m_fieldOfView;
         fov.OnEnterFOV.AddListener(OnEnterFOV);
         fov.OnExitFOV.AddListener(OnExitFOV);
     }
 
     private void InitDanmakus()
     {
-        m_danmakuList = m_danmakuList.Select(danmaku =>
-        {
-            danmaku = Instantiate(danmaku, transform);
-            danmaku.GetComponent<ProjectileEmitterAdvanced>().AutoFire = false;
-            return danmaku;
-        }).ToArray();
+        m_danmakuList = m_danmakuList.Select(danmaku => Instantiate(danmaku, transform)).ToArray();
     }
 
     private void OnEnterFOV(GameObject player)
     {
-        Debug.Log("Enter FOV");
         foreach (var danmaku in m_danmakuList)
         {
-            var emitter = danmaku.GetComponent<ProjectileEmitterAdvanced>();
-            emitter.AutoFire = true;
-            emitter.Target = player.transform;
+            foreach (var emitter in danmaku.GetComponents<ProjectileEmitterAdvanced>())
+            {
+                emitter.AutoFire = true;
+                emitter.Target = player.transform;
+            }
         }
     }
 
     private void OnExitFOV(GameObject player)
     {
-        Debug.Log("Exit FOV");
         foreach (var danmaku in m_danmakuList)
         {
-            var emitter = danmaku.GetComponent<ProjectileEmitterAdvanced>();
-            emitter.AutoFire = false;
+            foreach (var emitter in danmaku.GetComponents<ProjectileEmitterAdvanced>())
+            {
+                emitter.AutoFire = false;
+            }
         }
     }
 }
--- Assets/Scripts/Enemy/EnemyFOV.cs
+++ Assets/Scripts/Enemy/EnemyFOV.cs
@@ -4,17 +4,10 @@ using UnityEngine.Events;
 /// <summary>
 /// 敵の視野に入ったかどうかを事件で知らせる
 /// </summary>
-public class EnemyFOV : MonoBehaviour
+abstract public class EnemyFOV : MonoBehaviour
 {
     public readonly UnityEvent<GameObject> OnEnterFOV = new UnityEvent<GameObject>();
     public readonly UnityEvent<GameObject> OnExitFOV = new UnityEvent<GameObject>();
-
-    public float FOV
-    { 
-        get => GetComponent<CircleCollider2D>().radius;
-        internal set { GetComponent<CircleCollider2D>().radius = value; }
-    }
-
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
