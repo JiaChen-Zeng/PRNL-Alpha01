fb56af837bf895100ba2be2c30d7336d6a867b41
JiaChen ZENG <jiachen@zeng.games>
2022-01-08 01:44:17 +0800

    FEAT: カメラの位置を若干上へ設定できるようにする

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,51 +7,23 @@ public class CameraController : MonoBehaviour
     /// </summary>
     private const float k_mapWidth = 11.24f;
 
-    [SerializeField] private float m_constrainYMin = 0;
-    [SerializeField] private float m_constrainYMax = Mathf.Infinity;
     [SerializeField] private Transform m_target;
-    private Vector3 mNewPosition = new Vector3();
-  
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        
-    }
+
+    [SerializeField] private float constraintYMin = 0;
+    [SerializeField] private float constraintYMax = Mathf.Infinity;
+
+    [SerializeField] private float offsetY = 3;
 
     private void LateUpdate()
     {
-        DistanceCheck();
-        Follow();
+        TrackCharacter();
     }
 
-    private void Follow()
+    private void TrackCharacter()
     {
-        if(m_target.position.y > this.transform.position.y)
-        {
-            mNewPosition.x = this.transform.position.x;
-            mNewPosition.y = m_target.position.y;
-            mNewPosition.z = this.transform.position.z;
-            this.transform.position = mNewPosition;
-            SetNewPosition(this.transform.position.x, m_target.position.y, this.transform.position.z);
-        }
-    }
-
-    private void SetNewPosition(float x, float y, float z)
-    {
-        mNewPosition.x = x;
-        mNewPosition.y = Mathf.Clamp(y, m_constrainYMin, m_constrainYMax);
-        mNewPosition.z = z;
-        this.transform.position = mNewPosition;
-    }
-
-    private void DistanceCheck()
-    {
-        var direction = m_target.position - this.transform.position;
-        var dot = Vector3.Dot(m_target.up, direction.normalized);
-        if (dot < 0)
-        {
-            SetNewPosition(this.transform.position.x, m_target.position.y, this.transform.position.z);
-        }
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Clamp(m_target.position.y + offsetY, constraintYMin, constraintYMax);
+        transform.position = pos;
     }
 
     /// <summary>
