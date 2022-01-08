d4bbb4664fb0c8037120ec1f8a43f82eff243339
JiaChen ZENG <jiachen@zeng.games>
2022-01-09 00:28:38 +0800

    FIX: #31 ビルド環境でEmitterが3つほど超えると弾幕を発射しない

--- Assets/Unity-Bullet-Hell/Scripts/ProjectileManager.cs
+++ Assets/Unity-Bullet-Hell/Scripts/ProjectileManager.cs
@@ -285,9 +285,9 @@ namespace BulletHell
 
             // Provides a simple way to update active Emitters if removing/adding them at runtime for debugging purposes
             // You should be using AddEmitter() if you want to add Emitters at runtime (See above comment).
-#if UNITY_EDITOR
+//#if UNITY_EDITOR
             RefreshEmitters();
-#endif
+//#endif
 
             DrawEmitters();
             UpdateEmitters();               
